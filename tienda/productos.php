952b6222289c35462158c72e2e9474129965e259
JorgeIs-01 <infantessorolla@gmail.com>
2020-12-09 20:13:42 +0100

    alta productos

--- tienda/productos.php
+++ tienda/productos.php
@@ -1,5 +1,12 @@
+<?php
+require_once 'database.php';
+?>
+
+<link rel="stylesheet" href="./tienda.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-   
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <nav class="navbar navbar-expand-lg navbar-light bg-light navbar navbar-dark bg-dark">
   <a class="navbar-brand" href="#">Tienda</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
@@ -68,6 +75,21 @@
   </div>
 </nav>
 
+<br><br>
+<div class="dropdown">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      <?php
+      echo "Filtrar";
+      ?>
+    </button>
+    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+      <a class="dropdown-item" href="perfil.php">Precio ascendente</a>
+      <a class="dropdown-item" href="#">Precio descendente</a>
+      <a class="dropdown-item" href="#">Categorias</a>
+    </div>
+  </div>   
+
+</form>
 
 <table border="1" >
 		<tr>
@@ -91,10 +113,9 @@
                 <td><?php echo $mostrar['descripcion'] ?></td>
                 <td><?php echo $mostrar['oferta'] ?></td>
 				<td><?php echo $mostrar['precio'] ?></td>
-				<td><?php echo $mostrar['imagen'] ?></td>
+				<td><?php echo '<img src='.$mostrar['imagen'].' alt="" class="foto">' ?></td>
 			
 		</tr>
-		
 		<?php 
 			}
 		
